a8daacb4269fb53a979e93bfc3208fc65c48295a
shahzamalahmad <shahzamal2013@gmail.com>
2022-11-20 22:12:04 +0530

    first commit

--- db/migrate/20221114181523_remove.rb
+++ db/migrate/20221114181523_remove.rb
@@ -2,8 +2,8 @@ class Remove < ActiveRecord::Migration[7.0]
   def change
 
 
-    remove_column :articles, :mosque_ame
-    remove_column :articles, :adress
+    # remove_column :articles, :mosque_ame
+    # remove_column :articles, :adress
 
   end
 end
--- db/migrate/20221114181716_add.rb
+++
@@ -1,6 +0,0 @@
-class Add < ActiveRecord::Migration[7.0]
-  def change
-    remove_column :articles, :mosque_name
-    remove_column :articles, :address
-  end
-end
