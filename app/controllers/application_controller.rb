a59ad03eeb49d063b3ab5efb9ebe874b2d4732e4
shahzamalahmad <shahzamal2013@gmail.com>
2022-11-29 14:43:29 +0530

    role code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
 	before_action :authenticate_user!, except: [:index, :show, :about, :login]
+
+
+
+	 private
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :admin, :password, :email, :imam_name, :requirement, :user_id)
+    end
 end
